f7dcf3ec8415be9b83186b387bcf1806458bdce8
rwaaren <Rens.vanWaardenberg@gmail.com>
2020-01-21 12:54:30 +0100

    Update _.enqueue.lua

--- _.enqueue.lua
+++ _.enqueue.lua
@@ -1,5 +1,14 @@
--- _.enqueue.lua
+---
+-- Push value to first element of array
+---
+--@Usage local array = {1, 2, 3, 4}
+-- _.enqueue(array, 5)
+-- _print(array)
+-- --> {5, 1, 2, 3, 4}
 --
+-- @param array; Array to modify
+-- @param: value; Value to fill first element of Array with
+-- @return array
 _.enqueue = function (array, value)
     return table.insert(array, 1, value)
 end
